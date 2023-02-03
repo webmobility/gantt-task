d5866ebfc17664b98fbd57036a0a4e7bf3b2c0ad
kais <ho.ktayari@cma-cgm.com>
2023-02-03 16:09:37 +0100

    add tasks

--- stories/Gantt.tsx
+++ stories/Gantt.tsx
@@ -1,19 +1,48 @@
-import React from "react";
-import { Gantt } from "../src";
+import React, { useState } from "react";
+import { Column, Gantt, TaskOrEmpty, TitleColumn, ViewMode } from "../src";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { fr } from "date-fns/locale";
+import { initTasks } from "./helper";
+
+const columns: readonly Column[] = [
+  {
+    component: TitleColumn,
+    width: 350,
+    title: "Events",
+  },
+];
+
 export default () => {
+  const [tasks, setTasks] = useState<readonly TaskOrEmpty[]>(initTasks());
   return (
     <DndProvider backend={HTML5Backend}>
       <Gantt
+        viewMode={ViewMode.Hour}
+        canMoveTasks={false}
+        canResizeColumns={true}
+        columns={columns}
+        dateLocale={fr}
         tasks={[
           {
-            start: new Date(2020, 0, 1),
-            end: new Date(2020, 2, 2),
-            name: "Redesign website",
-            id: "Task 0",
-            progress: 45,
+            type: "project",
+            id: "PRESS",
+            name: "Activité Presse (1)",
+            start: new Date("2023-02-03T13:36:10.893Z"),
+            end: new Date("2023-02-03T13:36:10.893Z"),
+            progress: 100,
+            isDisabled: false,
+            hideChildren: false,
+          },
+          {
             type: "task",
+            id: "1",
+            name: "Préparation réunion Cash investigation",
+            start: new Date("2023-01-31T23:00:00.000Z"),
+            end: new Date("2023-02-02T11:30:00.000Z"),
+            parent: "PRESS",
+            progress: 0,
+            isDisabled: true,
           },
         ]}
       />
